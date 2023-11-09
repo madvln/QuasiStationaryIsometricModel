94a0c6ce5c43910cef4ed76e261f7878ea2a55d4
Егор Тараканчиков <egor200050@gmail.com>
2023-11-09 08:02:52 +0300

    utf 8 + comments

--- QuasiStationaryIsometricModel/test.h
+++ QuasiStationaryIsometricModel/test.h
@@ -2,55 +2,50 @@
 const double g = 9.8, pi = 3.14;
 class simple_equations;
 
-struct simple_struct {
-	double L; double D; double delta_d; double delta;
-	double z_0; double z_L; double rho; double nu; double p_n; double Q; double p_0; double p_L;
-};
-
 class simple_equations {
 public:
 
-	void count_d(double D, double delta_d)//������� ���������� �������
+	void count_d(double D, double delta_d)//считаем внутренний диаметр
 	{
 		diam_vnutr = (D - 2 * delta_d);
 	}
-	double get_d()//������� ���������� �������
+	double get_d()//выводим внутренний диаметр
 	{
 		return diam_vnutr;
 	}
 
-	void count_epsilon(double delta)//������� �������������
+	void count_epsilon(double delta)//считаем шероховатость
 	{
 		sherokh = delta / diam_vnutr;
 	}
-	double get_epsilon()//������� �������������
+	double get_epsilon()//выводим шероховатость
 	{
 		return sherokh;
 	}
 
-	void count_v(double Q)//������� ��������
+	void count_v(double Q)//считаем скорость
 	{
 		speed = (4 * Q) / (diam_vnutr * diam_vnutr * pi);
 	}
-	double get_v()//������� ��������
+	double get_v()//выводим скорость
 	{
 		return speed;
 	}
 
-	void count_Re(double nu)//������� ����� ����������
+	void count_Re(double nu)//считаем число Рейнольдса
 	{
 		Reynolds = (speed * diam_vnutr) / nu;
 	}
-	double get_Re()//������� ����� ����������
+	double get_Re()//выводим число Рейнольдса
 	{
 		return Reynolds;
 	}
 
-	void count_lambda()//������� �������������� �������������
+	void count_lambda()//считаем гидравлическое сопротивление
 	{
 		hydr_res = 0.11 * (pow((sherokh + (68.0 / Reynolds)), 0.25));
 	}
-	double get_lambda()//������� �������������� �������������
+	double get_lambda()//выводим гидравлическое сопротивление
 	{
 		return hydr_res;
 	}
@@ -58,21 +53,21 @@ public:
 	void count_p_0(double L, double D, double delta_d, double delta,
 		double z_0, double z_L, double rho, double nu, double p_n, double Q)//считаем давление на начале участка
 	{
-		
-		count_d(pimple_.D, pimple_.delta_d);
-		count_epsilon(pimple_.delta);
-		count_v(pimple_.Q);
-		count_Re(pimple_.nu);
+		count_d(D, delta_d);
+		count_epsilon(delta);
+		count_v(Q);
+		count_Re(nu);
 		count_lambda();
 		p_c_0 = ((p_n / (rho * g)) + z_L - z_0 + (hydr_res * L * speed * speed) / (diam_vnutr * 2 * g))
 			* (rho * g);
 	}
-	double get_p_0()
+	double get_p_0()//выводим давление на начале участка
 	{
 		return p_c_0;
 	}
 
-	void count_p_L(double L, double z_0, double z_L, double rho, double p_n)
+	void count_p_L(double L, double D, double delta_d, double delta,
+		double z_0, double z_L, double rho, double nu, double p_n, double Q)//считаем давление на конце участка
 	{
 		count_d(D, delta_d);
 		count_epsilon(delta);
@@ -82,13 +77,13 @@ public:
 		p_c_L = ((p_n / (rho * g)) - z_L + z_0 - (hydr_res * L * speed * speed) / (diam_vnutr * 2 * g))
 			* (rho * g);
 	}
-	double get_p_L()//������� �������� �� ����� �������
+	double get_p_L()//выводим давление на конце участка
 	{
 		return p_c_L;
 	}
 
 	void count_Q(double L, double D, double delta_d, double delta,
-		double z_0, double z_L, double rho, double nu, double p_0, double p_L)//������� ������
+		double z_0, double z_L, double rho, double nu, double p_0, double p_L)//считаем расход
 	{
 		count_d(D, delta_d);
 		count_epsilon(delta);
@@ -103,7 +98,7 @@ public:
 		} while (abs(hydr_res - lambda_approx) > 0.0002);
 		raskhod = pi * diam_vnutr * diam_vnutr * speed / 4;
 	}
-	double get_Q()//������� ������
+	double get_Q()//выводим расход
 	{
 		return raskhod;
 	}
@@ -118,25 +113,22 @@ private:
 
 TEST(MOC_Solver, Task_1)
 {
-	//��������� �������
+	//начальные условия
 	double L = 80e3, D = 0.72, delta_d = 0.01,
 		delta = 15e-6, z_0 = 50, z_L = 100,
-		rho = 870, nu = 15e-6, p_L = 0.6e6, Q = 0.972, p_0;
+		rho = 870, nu = 15e-6, p_L = 0.6e6, Q = 0.972;
 	simple_equations simple;
-	simple_struct pimple{ L,D,delta_d, delta, z_0, z_L, rho, nu, Q, p_0, p_L};
-	simple.count_p_0(pimple);
-	p_0 = simple.get_p_0();
+	simple.count_p_0(L, D, delta_d, delta, z_0, z_L, rho, nu, p_L, Q);
+	double p_0 = simple.get_p_0();
 }
 
 TEST(MOC_Solver, Task_2)
 {
-	//��������� �������
+	//начальные условия
 	double L = 80e3, D = 0.72, delta_d = 0.01,
 		delta = 15e-6, z_0 = 50, z_L = 100,
-		rho = 870, nu = 15e-6, Q, p_0 = 5e6, p_L = 0.8e6;
+		rho = 870, nu = 15e-6, p_0 = 5e6, p_L = 0.8e6;
 	simple_equations simple;
-	simple_struct pimple{L,D,delta_d, delta, z_0, z_L, rho, nu, Q, p_0, p_L};
-	pimple.D;
 	simple.count_Q(L, D, delta_d, delta, z_0, z_L, rho, nu, p_0, p_L);
-	Q = simple.get_Q();
+	double Q = simple.get_Q();
 }
